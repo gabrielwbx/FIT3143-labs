3ee21ab7d545425d3a2ed77d4e8da97abe182654
gabrielwbx <gwon0021@student.monash.edu>
2026-08-17 19:15:55 +0800

    LAB1 - Completed task 3 using openmp, developing based on task2's code

---
+++ Lab #1 (Week 4)/task3.c
@@ -0,0 +1,182 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>   
+#include <omp.h>
+#include <time.h>
+
+//
+// IMPORTANT: SET OMP_NUM_THREADS environment variable or use omp_set_num_threads()
+//
+#define NUM_THREADS 6
+
+int n;
+int above_hundred;
+FILE *fp = NULL;
+int *results[NUM_THREADS];
+int prime_count[NUM_THREADS];
+
+//function prototypes
+int prime(int num);
+void WriteToFile(FILE *pArg);
+
+int main() {
+    int i;
+    double time_taken;
+    struct timespec startComp, endComp;
+
+    printf("Enter number:\n");
+    scanf("%d", &n);
+
+    //checks if we need to write into a file or not
+    if (n >= 100) {
+        above_hundred = 1; //c dont allow boolean unless library, 0 is false and any non-zero is true
+    } else {
+        above_hundred = 0;
+    }
+
+    if (above_hundred == 1) {   
+        fp = fopen("prime_numberst3.txt", "w"); //open file for writing, if doesnt exist it will be created 
+
+        if (fp == NULL) {
+            printf("Error: Unable to open prime numbers file.\n");
+            return 1;
+        }
+    } else {
+        fp = stdout; //if not above 100, write to terminal
+    }
+
+    printf("Start computation\n");
+    clock_gettime(CLOCK_MONOTONIC, &startComp);
+
+    if (n <= 2) {
+        fprintf(fp, "No prime numbers found.\n"); //only special case
+    } else {
+        //array for each thread initially
+        for (i = 0; i < NUM_THREADS; i++) {
+            results[i] = NULL;
+            prime_count[i] = 0;
+        }
+
+        //parralel region starts here
+        #pragma omp parallel num_threads(NUM_THREADS)
+        {
+            int my_rank = omp_get_thread_num();
+            int count = 0;
+            struct timespec t_start, t_end;
+            double thread_time;
+
+            clock_gettime(CLOCK_MONOTONIC, &t_start);
+
+            printf("Thread %d created\n", my_rank);
+
+            // Calculate range for this thread
+            int npt = n / omp_get_num_threads(); // npt = numbers per thread
+            int nptr = n % omp_get_num_threads(); // nptr = numbers per thread remainder
+
+            int sp = my_rank * npt; // start point
+            int ep = sp + npt;      // end point
+
+            if (my_rank == omp_get_num_threads() - 1) {
+                ep += nptr; //last thread gets the remainder
+            }
+
+            int *prime_array = malloc((ep - sp) * sizeof(int));
+
+            //if 2 is within the threads range, add it first to ease incrementation
+            if (sp <= 2 && ep > 2) {
+                prime_array[count] = 2;
+                count++;
+            }
+
+            //determine where to strart incrementing
+            int start;
+            if (sp <= 3) {
+                start = 3;
+            } else if (sp % 2 == 0) {
+                start = sp + 1;  //goes to next off if the starting point is even
+            } else {
+                start = sp;      //already odd
+            }
+
+            //checks odd numbers only, increment by 2
+            for (int i = start; i < ep; i += 2) {
+                if (prime(i)) {
+                    prime_array[count] = i;
+                    count++;
+                }
+            }
+
+            results[my_rank] = prime_array;
+            prime_count[my_rank] = count;
+
+            clock_gettime(CLOCK_MONOTONIC, &t_end);
+
+            thread_time = (t_end.tv_sec - t_start.tv_sec) * 1e9; 
+            thread_time = (thread_time + (t_end.tv_nsec - t_start.tv_nsec)) * 1e-9; 
+            printf("Thread %d finished. Computational time: %fs\n", my_rank, thread_time);
+        } //exit parralel region
+
+        WriteToFile(fp);
+    }
+
+    // Free allocated memory
+    for (int t = 0; t < NUM_THREADS; t++) {
+        if (results[t] != NULL) {
+            free(results[t]);
+        }
+    }
+
+    clock_gettime(CLOCK_MONOTONIC, &endComp);
+
+    //taken from lab week 3
+    time_taken = (endComp.tv_sec - startComp.tv_sec) * 1e9; 
+    time_taken = (time_taken + (endComp.tv_nsec - startComp.tv_nsec)) * 1e-9; 
+
+    if (above_hundred == 1) {
+        fclose(fp); 
+        printf("Prime numbers written to prime_numberst3.txt\n");
+    }
+
+    printf("\nEnd computation\n");
+    printf("\nComputational time: %f seconds\n", time_taken);
+
+    return 0;
+}
+
+int prime(int num)
+{
+    if (num == 2) {
+        return 1;
+    }
+
+    if (num < 2) {
+        return 0;
+    }
+
+    if (num % 2 == 0) {
+        return 0;  //if u divide 2 and theres no remainder it means its an even number and not prime
+    }
+ 
+    // only need to check divisors up to sqrt(num)
+    int sqrt_num = (int)sqrt(num);
+    for (int x = 3; x <= sqrt_num; x += 2) { //skip even numbers
+        if (num % x == 0) {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+void WriteToFile(FILE *pArg) {
+    int primes_per_line = 0;
+ 
+    for (int t = 0; t < NUM_THREADS; t++) {
+        for (int j = 0; j < prime_count[t]; j++) {
+            if (primes_per_line > 0) {
+                fprintf(pArg, "%s", (primes_per_line % 25 == 0) ? ",\n" : ", "); //introduced ternary operators so it doesnt look too clustered
+            }
+            fprintf(pArg, "%d", results[t][j]);
+            primes_per_line++;
+        }
+    }
+}
