53e079238e68385099eab896af5c89231fd486e5
gabrielwbx <gwon0021@student.monash.edu>
2026-08-18 13:36:53 +0800

    LAB1 - Updated some documentation for task1.c

--- Lab #1 (Week 4)/task1.c
+++ Lab #1 (Week 4)/task1.c
@@ -1,18 +1,27 @@
+/*
+* task1.c
+* How to run file:
+* 1. Open terminal for Lab #1 (Week 4) file directory
+* 2. Compile the file using 'gcc -o task1 task1.c'
+* 3. Run the .exe using './task1'
+*/
+
+
 #include <stdio.h> 
 #include <math.h>
 #include <stdlib.h>
 #include <time.h>
 
+//global variables
 int above_hundred;
 FILE *fp = NULL;
 int *prime_array;
 int prime_count = 0;
 
-//function prototype 
+//function prototypes
 int prime(int num);
 void WriteToFile(FILE *pArg); 
 
-// Main function (no counter)
 int main() {
     int n;
     double time_taken;
@@ -45,7 +54,7 @@ int main() {
     if (n <= 2) {
         fprintf(fp, "No prime numbers found.\n");
     } else {
-        prime_array = malloc(n * sizeof(int));
+        prime_array = malloc(n * sizeof(int)); //allocates memory for array of size n * 4 (integer bytes)
         
         prime_array[prime_count] = 2;
         prime_count++;
@@ -60,6 +69,7 @@ int main() {
 
     WriteToFile(fp);
 
+    //free the array for memory
     if (prime_count > 0) {
         free(prime_array);
     }
@@ -105,12 +115,13 @@ int prime(int num)
     return 1;
 }
 
+//writing to file function, takes in a file pointer as an argument
 void WriteToFile(FILE *pArg) {
     int primes_per_line = 0;
 
     for (int j = 0; j < prime_count; j++) {
         if (primes_per_line > 0) {
-            fprintf(pArg, "%s", (primes_per_line % 25 == 0) ? ",\n" : ", ");
+            fprintf(pArg, "%s", (primes_per_line % 25 == 0) ? ",\n" : ", "); //ternary operators used for reducing clutter
         }
         fprintf(pArg, "%d", prime_array[j]);
         primes_per_line++;
